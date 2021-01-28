35bd124b2a4631b0274bb157cae560b6bf398661
3615Jam <garciajeanmarc@gmail.com>
2021-01-28 02:37:44 +0100

    jam central

--- cp7/functions.php
+++ cp7/functions.php
@@ -6,9 +6,9 @@
  *  
  * renvoie l'âge en années entre 2 dates passées en paramètres 
  * 
- * @param {string} $date1 - date
- * @param {string} $date2 - une autre date
- * @return {int} âge en années 
+ * @param   string  $date1  une 1ere date
+ * @param   string  $date2  une 2eme date
+ * @return  int             l'âge en années 
  * 
  * */   
 
@@ -17,7 +17,6 @@ function calculAge($date1, $date2):int {
     if(!isDate($date1) || !isDate($date2)) {
         trigger_error("l'un des 2 arguments n'est pas une date", E_USER_WARNING);
     }
-
     // au cas où on aura pas une string, on convertit les dates en string
     $d1 = strtotime($date1);
     $d2 = strtotime($date2);
@@ -27,8 +26,15 @@ function calculAge($date1, $date2):int {
     } elseif($d2 < $d1) {
         $diffDate = $d1 - $d2;
     } else {
+        // si les dates sont les mêmes 
         $diffDate = 0;
-    };  
+    };
+    // $diffDate renvoie une valeur en secondes, on la divise 
+    // donc par 60 pour passer en minutes
+    // puis par 60 pour passer en heures
+    // puis par 24 pour passer en jours
+    // puis par 365.25 pour passer en années 
+    // puis on 'floor' le tout pour arrondir à l'entier inférieur 
     return floor($diffDate / 60 / 60 / 24 / 365.25);
 }
 
@@ -39,8 +45,8 @@ function calculAge($date1, $date2):int {
  *  
  * vérifie si la date est bien une date 
  * 
- * @param {string} $arg - argument à tester
- * @return {boolean} hjkfldsq
+ * @param   string  $arg l'élément à tester
+ * @return  bool    vrai ou faux : est-ce une date ? 
  * 
  * */
 
@@ -51,25 +57,26 @@ function isDate($arg) : bool {
 
 /** 
  * 
- * fonction "ttc" :
+ * fonction "calculTtc" :
  *  
  * calculer le montant TTC à partir d'un montant HT 
  * 
- * @param {float} $mt - montant, nombre réel positif ou nul 
- * @param {float} $taux - taux de tva utilisé, parmi 3 définis (5.5, 10 et 20%, 20% étant la valeur par défaut)
- * @return {float} $xxxxx - 
+ * @param   int|float   $mtht   montant HT, nombre réel positif ou nul 
+ * @param   float       $taux   taux de tva utilisé, parmi 3 définis (5.5, 10 et 20%, 20% étant la valeur par défaut)
+ * @return  int|float           montant TTC en fonction du taux appliqué 
  * 
  * */
 
-function ttc($mt, $taux = 0.2) {
+function calculTtc($mtht, $taux = 0.2) {
     $tauxFR = [0.021, 0.055, 0.1, 0.2];
-    if($mt < 0 && !is_float($mt)) {
+    if(is_string($mtht)) {
+        trigger_error("Le montant doit être un nombre", E_USER_WARNING);
+    } elseif($mtht < 0) {
         trigger_error("Le montant doit être positif", E_USER_WARNING);
-    } elseif(!in_array($taux, $tauxFR, true)) {
-        trigger_error("La valeur de la TVA doit être l'une de ces valeurs : " . implode(', ', $tauxFR), E_USER_WARNING);
+    } elseif(!in_array($taux, $tauxFR)) {
+        trigger_error("Le taux ne peut avoir que l'une de ces valeurs : " . implode(', ', $tauxFR), E_USER_WARNING);
     } else {
-    $ttcMt = $mt * (1 + $taux);
-    return $ttcMt;
+    return $mtht * (1 + $taux);
     }
 }
 
--- cp7/test_functions.php
+++ cp7/test_functions.php
@@ -13,15 +13,17 @@ echo '<p> Test 5 : ' . isDate("Toto aime les gateaux");
 echo '<p> Test 6 : ' . isDate(1234323);
 echo '<p> Test 7 : ' . isDate("29/02/2021");
 
-// test de la fonction 'ttc' : 
-echo '<p> Test 8 : ' . ttc(100);
-echo '<p> Test 9 : ' . ttc(100, 0.021);
-echo '<p> Test 10 : ' . ttc(100, 0.055);
-echo '<p> Test 11 : ' . ttc(100, 0.1);
-echo '<p> Test 12 : ' . ttc(100, 0.123);
-echo '<p> Test 13 : ' . ttc(-100, 0.055);
-echo '<p> Test 14 : ' . ttc("100", "0.055");
-echo '<p> Test 14 : ' . ttc("yuerioza", 0.055);
-
+// test de la fonction 'calculTtc' : 
+echo '<p> Test 8 : ' . calculTtc(100);
+echo '<p> Test 9 : ' . calculTtc(100, 0.021);
+echo '<p> Test 10 : ' . calculTtc(100, 0.055);
+echo '<p> Test 11 : ' . calculTtc(100, 0.1);
+echo '<p> Test 12 : ' . calculTtc(100, 0.123);
+echo '<p> Test 13 : ' . calculTtc(-100, 0.055);
+echo '<p> Test 14 : ' . calculTtc("100", 0.055);
+echo '<p> Test 15 : ' . calculTtc(100, "0.055");
+echo '<p> Test 16 : ' . calculTtc("100", "0.055");
+echo '<p> Test 17 : ' . calculTtc("yuerioza", 0.055);
+echo '<p> Test 18 : ' . calculTtc(100, "huofyzar");
 
 ?>
