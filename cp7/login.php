c80fd4f01695ad23f58f72fe17d6d981795e3619
3615Jam <garciajeanmarc@gmail.com>
2021-02-16 16:29:48 +0100

    login.php

--- cp7/login.php
+++ cp7/login.php
@@ -2,8 +2,37 @@
 include_once('constants.php');
 
 try {
+    // verif $_POST 
+    if (isset($_POST['mail']) && !empty($_POST['mail'])) {
+    }
+
+
+
+
+
+
+
+
+    // connexion BDD via PDO 
     $cnn = new PDO('mysql:host=' . HOST . ';dbname=' . DB, USER, PASS);
     $cnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // query select count * = 1 
+
+
+
+
+
+
+
+    // si user connu --> bo.php
+    if ("user existe") {
+        header('location:bo.php');
+    }
+    // sinon --> index.php?user=ko 
+    else {
+        header('location:index.php?user=ko');
+    }
 } catch (Exception $e) {
     echo '<p class="alert alert-danger">ERREUR : ' . $e->getMessage() . '</p>';
 }
