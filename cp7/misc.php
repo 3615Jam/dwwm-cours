b23a856a1ad8acd7905f01234d9215c70daef30c
Jean-Marc <garciajeanmarc@gmail.com>
2021-05-18 16:50:08 +0200

    add misc.php

---
+++ cp7/misc.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Darons Codeurs | Centres de vaccination</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</head>
+
+<body class="container">
+
+    <h1>Centres de vaccination par département</h1>
+
+    <?php
+
+    $deps = json_decode(file_get_contents("https://geo.api.gouv.fr/departements"));
+    $html =
+
+
+        $json = file_get_contents("https://www.data.gouv.fr/fr/datasets/r/d0566522-604d-4af6-be44-a26eefa01756");
+    $json = json_decode($json);
+    echo '<pre>';
+    var_dump($json);
+    echo '</pre>';
+
+    ?>
+
+</body>
+
+</html>
