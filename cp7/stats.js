46705bc22896bdb8719db08c1cbfc00e38d5faa8
Jean-Marc <garciajeanmarc@gmail.com>
2021-06-02 15:43:45 +0200

    CP7 minor changes

--- cp7/stats.js
+++ cp7/stats.js
@@ -6,10 +6,5 @@ document.getElementById('year').addEventListener('change', redrawChart);
 
 // on change la source de 'chart.php' avec les paramètres dans URL
 function redrawChart() {
-    // let eYear = document.getElementById('year');
-    // let eEmp = document.getElementById('emp');
-    // let eChart = document.getElementById('chart');
-    // eChart.src = 'chart.php?e=' + eEmp.value + '&a=' + eYear.value;
-
     document.getElementById('chart').src = 'chart.php?e=' + document.getElementById('emp').value + '&a=' + document.getElementById('year').value;
 }
